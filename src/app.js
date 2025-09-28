28c79ac54029e0dfc2f974fd872fe76305e41547
Josh Herald Abelardo <joshheraldabelardo@Ikyangs-MacBook-Pro.local>
2025-09-29 06:48:09 +0800

    Started with the postman api platform and got the gist on how axios work and learnt get and post endpoints

--- src/app.js
+++ src/app.js
@@ -1,21 +1,41 @@
 const express = require('express')
+const { join  } = require('path')
+
+const { people } = require('./db')
+
 const app = express()
-const path = require('path')
 
-const PORT = 8080
-const pathFile = path.join(__dirname, '..', 'public')
 
-app.use(express.static(pathFile))
+const PORT = 8080;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(join(__dirname, '..', 'public')))
+app.use(express.json())
+
+app.post('/login', (req, res) => {
+    const { name } = req.body
+    if(name) {
+        res.status(200).send(`HELLO ${name}`)
+        
+    }
+    res.status(401).send('Please provide credentials')
 })
 
-app.use((req, res) => {
-    res.status(404).send('Not found')
+app.get('/api/people', (req, res) => {
+    res.send({success: true, data: people})
+}) 
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+    if(!name) {
+        res.status(401).send({success: false, msg: 'please provide name'})
+    }
+
+    res.status(201).json({success: true, person: name})
 })
 
-app.listen(PORT, (req, res) => {
-    console.log(`Listening to PORT ${PORT}`)
-})
 
+
+app.listen(PORT, () => {
+    console.log(`Listening in PORT ${PORT}`)
+})
