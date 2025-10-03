5ad05d007a0e12536fbc819761d1e77f5ab8335d
Josh Herald Abelardo <joshheraldabelardo@Ikyangs-MacBook-Pro.local>
2025-10-03 14:49:00 +0800

    Added a put method in API

--- src/app.js
+++ src/app.js
@@ -21,10 +21,29 @@ app.get('/api/insomnia/people', (req, res) => {
 app.post('/api/insomnia/people', (req, res) => {
     const { name } = req.body
 
-    if(!name) {
+    if(!name) {ter
         return res.status(401).json({success: false, data: 'No input allowed'})
     }
     res.status(200).json({success: true, data: name})
+})
+
+app.put('/api/insomnia/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    const newPerson = people.find((person) => person.id === Number(id))
+    if(!newPerson) {
+        return res
+        .status(404)
+        .json({success: false, data: `${id} could not be found`})
+    }
+
+    const changeName = people.map((person) => {
+        if(person.id === Number(id)) {
+            person.name = name
+        }
+    })
+    res.status(200).json({success: true, data: people})
 })
 
 app.listen(PORT, () => {
